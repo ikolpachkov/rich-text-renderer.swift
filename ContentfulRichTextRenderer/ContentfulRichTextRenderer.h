91d6cfe5913c77875bc12ed8ec39d5b4729f195e
i.kolpachkov <i.kolpachkov@gmail.com>
2018-12-24 15:17:51 +0300

    add iOS shared framework for carthage building

---
+++ ContentfulRichTextRenderer/ContentfulRichTextRenderer.h
@@ -0,0 +1,19 @@
+//
+//  ContentfulRichTextRenderer.h
+//  ContentfulRichTextRenderer
+//
+//  Created by Xorosho on 12/24/18.
+//  Copyright © 2018 Ovenbits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContentfulRichTextRenderer.
+FOUNDATION_EXPORT double ContentfulRichTextRendererVersionNumber;
+
+//! Project version string for ContentfulRichTextRenderer.
+FOUNDATION_EXPORT const unsigned char ContentfulRichTextRendererVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContentfulRichTextRenderer/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+use_frameworks!
+platform :ios, "9.3"
+
+target 'ContentfulRichTextRenderer' do
+  pod 'Contentful'
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+end
+
